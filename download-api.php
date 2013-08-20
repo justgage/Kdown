27dcfb8216294d899ef3a3df6a858931f8be1086
Gage Peterson <justgage@gmail.com>
2013-08-20 15:19:00 -0600

    Started adding the Ajax to the Market select
    Also redid the json structure to make more sense in javascript.

---
+++ download-api.php
@@ -0,0 +1,26 @@
+<?php 
+
+$response = array();
+
+if (isset($_POST['market'])) {
+    $json = file_get_contents("info.json");
+    $markets = json_decode($json, true);
+
+    if ( isset( $markets[ $_POST['market'] ] ) ) {
+
+        $categorys = array("categorys" => $markets[$_POST['market']]);
+
+        $response = array_merge(array("error" => false) , $categorys );
+    }
+    else {
+        $response = array("error" => true);
+    }
+} 
+else { // if the data was sent wrong
+    $response = array("error" => true);
+}
+
+
+echo json_encode($response);
+
+?>
