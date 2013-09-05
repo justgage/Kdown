63f3b2fccddf6a4409c3c39cd063660aad3a3c32
Gage Peterson <justgage@gmail.com>
2013-09-05 09:59:01 -0600

    Got rid of the POST testing file and changed some of the names of constants

--- downloads.js
+++ downloads.js
@@ -4,24 +4,22 @@ $(function () {
      * download ui object
      */
 
-
     var dl_ui = dl_ui || {};
 
     //these are the selectors for different elements on the page 
-    dl_ui.sel_dropdown      = "#market_select";
-    dl_ui.sel_current_cat = ".current_page_item";
-    dl_ui.cat_links     = "#vertical_nav ul li a";
+    dl_ui.SEL_MARKET      = "#market_select";
+    dl_ui.SEL_CAT_CURRENT    = ".current_page_item";
+    dl_ui.SEL_CAT_LINK     = "#vertical_nav ul li a";
 
     dl_ui.bind = function() {
 
         //click the category changes it
-        $(dl_ui.cat_links).click(function(){
-            $(dl_ui.sel_current_cat).removeAttr('class');
-            $(this).parent().attr('class', dl_ui.sel_current_cat.slice(1));
+        $(dl_ui.SEL_CAT_LINK).click(function(){
+            $(dl_ui.SEL_CAT_CURRENT ).removeAttr('class');
+            $(this).parent().attr('class', dl_ui.SEL_CAT_CURRENT    .slice(1));
             dl_ui.ajax_load();
         });
     }
-
 
     //load category list
     $.post("api.php", { "cat-list":""},  function(json){
@@ -30,37 +28,34 @@ $(function () {
         var item = $("#vertical_nav li"); $(item).remove();
 
         for (var i = 0, l = json.list.length; i < l; i ++) {
-
-
             var temp = item.clone(); 
-
             var menu_item = $("#vertical_nav ul").append(temp);
-
             $("#vertical_nav a").last().text(json.list[i]);
-             
         }
 
-        $("#vertical_nav li").first().attr('class', dl_ui.sel_current_cat.slice(1));
-
+        $("#vertical_nav li").first().attr('class', dl_ui.SEL_CAT_CURRENT   .slice(1));
         dl_ui.bind();
         dl_ui.ajax_load();
 
-    }, "json");
-    
+    }, "json"); // JSON! very important to include this 
+
 
     /***
      * Bind market select to reloading the list of files
      */
-    $(dl_ui.sel_dropdown).change(
+    $(dl_ui.SEL_MARKET).change(
         function () {
         dl_ui.ajax_load();
     });
 
 
+    /***
+     * This is a function that will 
+     */
     dl_ui.ajax_load =  function() {
 
-        var market = $(dl_ui.sel_dropdown).val();
-        var cat = $(dl_ui.sel_current_cat).text();
+        var market = $(dl_ui.SEL_MARKET).val();
+        var cat = $(dl_ui.SEL_CAT_CURRENT   ).text();
         //empty the list of items
         $('#dl_table').html("<h3><em>Download list loading...</em></h3>");
         $('.none-found').show();
@@ -88,20 +83,17 @@ $(function () {
             //make a copy and get rid of it
             var row = $("#table_copy"); row.remove();
 
+            // if the category exists in the data
             if (json.cats) {
                 for (var i = 0, l = json.cats.length; i < l; i ++) {
 
                     var file = json.cats[i];
-
-                    //other stuff
-
                     var newRow = row.clone();
-                    var copy = $("#dl_table table").append(newRow);
+                    $("#dl_table table").append(newRow);
 
-                    //replace all values in the file
+                    //Put the files information into the new row in the table
                     $(newRow).find(".table_star").text( "*" );
-                    $(newRow).find(".table_name a").text( file.filename );
-                    $(newRow).find(".table_name a").attr('href', "single.php?id=" + encodeURIComponent( file.id ));
+                    $(newRow).find(".table_name a").text( file.filename ).attr('href', "single.php?id=" + encodeURIComponent( file.id ));
                     $(newRow).find(".table_lang").text("---");
                     $(newRow).find(".table_dl_link a").attr('href', file.href);
 
@@ -114,13 +106,5 @@ $(function () {
 
 
     };
-
-
-
-
-
-
-
-
 
 });
